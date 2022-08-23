8c24a49f5d6f9e3cb7a7d8224d01b9ead8bf35fa
nightmare117 <iftekherfuad@gmail.com>
2022-08-23 18:39:32 +0600

    app init

---
+++ MyPortfolioWebsite/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Nav from './components/nav.vue'
+</script>
+
+<template>
+
+  <div>
+    <Nav />
+    <div>
+      <RouterView />
+    </div>
+  </div>
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header> -->
+</template>
+
+<style>
+body{
+  margin: 0% !important;
+}
+</style>
---
+++ MyPortfolioWebsite/src/components/nav.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="nav">
+
+    </div>
+</template>
+
+<style>
+.nav{
+    height: 60px;
+    width: 100%;
+    background: rgba(250,143,61,1);
+}
+</style>
